f3d86d8ec40418e3391352ca5429596183eb6ee1
ritonun <73615644+ritonun@users.noreply.github.com>
2025-03-01 12:17:21 +0100

    better naming and clearer code

--- examples/api_call.rs
+++ examples/api_call.rs
@@ -2,15 +2,12 @@ use f1_api as f1;
 
 #[tokio::main]
 async fn main() {
-    println!("Get session");
-
+    // Create a client to make rewest to openf1.org
     let openf1 = f1::OpenF1Client::new();
+
+    // get all sessions info
     let sessions = match openf1.get_sessions().await {
         Ok(response) => response,
         Err(e) => panic!("Error getting session due to {}", e),
     };
-
-    for s in sessions {
-        println!("{:?}", s);
-    }
 }
